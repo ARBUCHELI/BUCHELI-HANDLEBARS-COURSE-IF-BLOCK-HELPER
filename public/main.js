0982f57d030610fd76c3078ed9914e61a8c0686d
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-01-14 14:15:31 +0300

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,13 @@
+const source = document.getElementById('ifHelper').innerHTML;
+const template = Handlebars.compile(source);
+
+const context = {
+  opinion: true
+}
+
+const compiledHtml = template(context);
+
+const debateElement = document.getElementById('debate');
+
+debateElement.innerHTML = compiledHtml;
+
